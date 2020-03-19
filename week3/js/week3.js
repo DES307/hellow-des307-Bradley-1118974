a0f876c223b192999aa827625d57afe9caeaffea
Bradley <Brad@Bradleys-MacBook-Air.local>
2020-03-19 11:02:55 +1000

    End of Week 3

--- week3/js/week3.js
+++ week3/js/week3.js
@@ -34,47 +34,50 @@
 
 // console.log(person.address.city);
 
-const me = {
-    firstName: 'Brad',
-    lastName: 'Hart',
-    age: 24,
-    gender: 'male',
-    hobbies: ['movie', ' gaming', ' motorsport'],
-    address: {
-        number: 4,
-        street: 'Esprit',
-        city: 'Birtinya',
-        postcode: 4575,
-        state: 'QLD'
-    }
-};
+// const me = {
+//     firstName: 'Brad',
+//     lastName: 'Hart',
+//     age: 24,
+//     gender: 'male',
+//     hobbies: ['movie', ' gaming', ' motorsport'],
+//     address: {
+//         number: 4,
+//         street: 'Esprit',
+//         city: 'Birtinya',
+//         postcode: 4575,
+//         state: 'QLD'
+//     }
+// };
 
-console.log(`My name is ${me.firstName} ${me.lastName}\nI am a ${me.age} year old ${me.gender}\nI live at: ${me.address.number} ${me.address.street}, ${me.address.city}, ${me.address.postcode}, ${me.address.state}\nMy hobbies are: ${me.hobbies}`)
-console.log(me.hobbies[2].toUpperCase());
-console.log(me.firstName.length);
+// console.log(`My name is ${me.firstName} ${me.lastName}\nI am a ${me.age} year old ${me.gender}\nI live at: ${me.address.number} ${me.address.street}, ${me.address.city}, ${me.address.postcode}, ${me.address.state}\nMy hobbies are: ${me.hobbies}`)
+// console.log(me.hobbies[2].toUpperCase());
+// console.log(me.firstName.length);
 
 // ------- if
 
-let score = 88;
+// let score = 88;
 
-score > 50 ? console.log('passed') : console.log ('failed');
+// score > 50 ? console.log('passed') : console.log ('failed');
 
-if (score > 75 && score < 95) {
-    console.log('good');
-} else if (score > 50) {
-    console.log('pass');
-} else {
-    console.log('fail');
-}
+// if (score > 75 && score < 95) {
+//     console.log('good');
+// } else if (score > 50) {
+//     console.log('pass');
+// } else {
+//     console.log('fail');
+// }
 
-let age = 21;
+// let age = 21;
 
-if (age >= 20 && age < 30) {
-    console.log('young');
-} else if (age > 30 && age < 50) {
-    console.log('strong'); 
-    } else {
-        console.log('experienced');
-    }
+// if (age >= 20 && age < 30) {
+//     console.log('young');
+// } else if (age > 30 && age < 50) {
+//     console.log('strong'); 
+//     } else {
+//         console.log('experienced');
+//     }
 
 // did not work // age > 41 && age < 30 ? console.log('young') : age > 40 ? console.log('2') : console.log('3'); 
+
+//------------ Week 4 --------------
+
