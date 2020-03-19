70f0e7392d8c191a0dd8f668f412c24241a33a5a
Bradley <Brad@Bradleys-MacBook-Air.local>
2020-03-19 10:19:17 +1000

    Week 3

---
+++ week3/js/week3.js
@@ -0,0 +1,80 @@
+//object.method
+// console.log('test');
+
+//---variable
+//var
+
+// let number = 10;
+
+// console.log(number);
+
+//const
+
+// primtive data types: string, number, bollean, null, underfined
+
+// const firstName = 'brad';
+// const lastName = 'hart';
+// const age = 20;
+// const marrage = false;
+
+// console.log(`my name is: ${firstName} ${lastName} and I am ${age}.\nMarrage status: ${marrage}`);
+// console.log(firstName.toUpperCase());
+
+// const person = {
+//     firstName: 'brad',
+//     lastName: 'hart',
+//     age: 20,
+//     marrage: false,
+//     address: {
+//         number: 4,
+//         street: 'esprit',
+//         city: 'sippy downs'
+//     }
+// };
+
+// console.log(person.address.city);
+
+const me = {
+    firstName: 'Brad',
+    lastName: 'Hart',
+    age: 24,
+    gender: 'male',
+    hobbies: ['movie', ' gaming', ' motorsport'],
+    address: {
+        number: 4,
+        street: 'Esprit',
+        city: 'Birtinya',
+        postcode: 4575,
+        state: 'QLD'
+    }
+};
+
+console.log(`My name is ${me.firstName} ${me.lastName}\nI am a ${me.age} year old ${me.gender}\nI live at: ${me.address.number} ${me.address.street}, ${me.address.city}, ${me.address.postcode}, ${me.address.state}\nMy hobbies are: ${me.hobbies}`)
+console.log(me.hobbies[2].toUpperCase());
+console.log(me.firstName.length);
+
+// ------- if
+
+let score = 88;
+
+score > 50 ? console.log('passed') : console.log ('failed');
+
+if (score > 75 && score < 95) {
+    console.log('good');
+} else if (score > 50) {
+    console.log('pass');
+} else {
+    console.log('fail');
+}
+
+let age = 21;
+
+if (age >= 20 && age < 30) {
+    console.log('young');
+} else if (age > 30 && age < 50) {
+    console.log('strong'); 
+    } else {
+        console.log('experienced');
+    }
+
+// did not work // age > 41 && age < 30 ? console.log('young') : age > 40 ? console.log('2') : console.log('3'); 
