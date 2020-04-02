47ebd6a646d56850a02260a2d8efc688ae6ae4d4
Bradley <Brad@brads-macbook.modem>
2020-04-02 19:43:02 +1000

    Exercise 2
    early

---
+++ Assessment_Tasks/Task_2/Exercise_2/js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+    // $('#slideshow .slick').slick( {
+    //     autoplay: true,
+    //     autoplaySpeed: 2000,
+    //     speed: 2000,
+    //     dots: true,
+    //     arrows: false,
+    // });
+
+    $('.slick').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+      });
+          
+});
