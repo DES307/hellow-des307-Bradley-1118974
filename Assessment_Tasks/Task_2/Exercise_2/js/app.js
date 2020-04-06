fa29d9f1324b22df25c8f19e3b393b34d6c6e7d7
Bradley <Brad@brads-macbook.modem>
2020-04-06 20:30:34 +1000

    Exercise 2
    Structural framework complete

--- Assessment_Tasks/Task_2/Exercise_2/js/app.js
+++ Assessment_Tasks/Task_2/Exercise_2/js/app.js
@@ -11,8 +11,66 @@ $(document).ready(() => {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 2000,
         arrows: false,
       });
           
 });
+
+// -------- slider 2 -----------
+$(document).ready(() => {
+  $('.slick2').slick({
+    slidesToShow:  1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  });
+});
+
+// -------- slider 3 -----------
+// $(document).ready(() => {
+//   $('.slick3').slick({
+//     dots: false,
+//     arrows: false,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 3,
+//     autoplay: true,
+//     centerMode: true,
+//     variableWidth: true
+//   });
+// });
+
+$(document).ready(() => {
+  $('.slick3').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+});
+
+
+// -------- notes -----------
+console.log('All images are sourced from Unsplash: https://unsplash.com');
